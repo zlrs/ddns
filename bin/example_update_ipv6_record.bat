d449fe8b4487b0b684813e9e6fca4afb0cde6720
Karl Zhu <zhuys123@gmail.com>
2023-10-09 21:36:45 +0800

    example logging to file

--- bin/example_update_ipv6_record.bat
+++ bin/example_update_ipv6_record.bat
@@ -1,1 +1,4 @@
+@echo
+cd /d %~dp0
+cd /d ..
 python -m bin.example_update_ipv6_record
--- bin/exmaple_update_ipv6_record.py
+++ bin/exmaple_update_ipv6_record.py
@@ -1,27 +1,34 @@
+import sys
+import logging
 from public_address import get_public_ipv6_address
 from dnspod_ddns import DnsPodCN_DDNSClient
 
-domain = 'my_server.com'
+domain = 'google.com'
 subdomain = 'www'
-login_token = '<dnspodcn_api_token>'
-client = DnsPodCN_DDNSClient(login_token)
+login_token = 'xxxxxx'
+log_filename = 'log.txt'
 
+FORMTAT = '%(asctime)s %(message)s'
+logging.basicConfig(format=FORMTAT, filename=log_filename, encoding='utf-8', level=logging.DEBUG)
+
+logging.info('===== Start =====')
 my_ipv6_addr = get_public_ipv6_address()
 if my_ipv6_addr == '':
-    print('WARNING: No IPv6 public address found. Exit now...')
-    exit(1)
+    logging.info('No IPv6 public address found. Exit now...')
+    sys.exit(1)
 
+client = DnsPodCN_DDNSClient(login_token)
 record_ipv6_addr = client.get_AAAA_record_IPv6_address(subdomain=subdomain, domain_name=domain)
 if record_ipv6_addr == '':
-    print('WARNING: Can not get record from DDNS API. Exit now...')
-    exit(2)
+    logging.warning('Can not get record from DDNS API. Exit now...')
+    sys.exit(2)
 
 if my_ipv6_addr == record_ipv6_addr:
-    print(f'INFO: Address is not changed {my_ipv6_addr}. No need to update.')
-    exit(0)
+    logging.warning(f'Address is not changed {my_ipv6_addr}. No need to update.')
+    sys.exit(0)
 
 success = client.update_AAAA_record(my_ipv6_addr, subdomain=subdomain, domain_name=domain)
 if success:
-    print(f'SUCCESS: Updated the AAAA record for {subdomain}.{domain} to {my_ipv6_addr}')
+    logging.info(f'SUCCESS: Updated the AAAA record for {subdomain}.{domain} to {my_ipv6_addr}')
 else:
-    print(f'ERROR: Failed to update the AAAA record for {subdomain}.{domain} to {my_ipv6_addr}.')
+    logging.info(f'ERROR: Failed to update the AAAA record for {subdomain}.{domain} to {my_ipv6_addr}.')
