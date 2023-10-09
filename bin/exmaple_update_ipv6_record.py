64666032c67204b9b2da612298547e2cc14fa86e
Karl Zhu <zhuys123@gmail.com>
2023-10-09 21:52:34 +0800

    Log to stdout and file

--- bin/exmaple_update_ipv6_record.py
+++ bin/exmaple_update_ipv6_record.py
@@ -8,27 +8,51 @@ subdomain = 'www'
 login_token = 'xxxxxx'
 log_filename = 'log.txt'
 
-FORMTAT = '%(asctime)s %(message)s'
-logging.basicConfig(format=FORMTAT, filename=log_filename, encoding='utf-8', level=logging.DEBUG)
+logger = logging.getLogger()
+formatter = logging.Formatter('%(asctime)s %(message)s')
 
-logging.info('===== Start =====')
+
+def get_stream_handler():
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setFormatter(formatter)
+    return stream_handler
+
+
+def get_file_handler():
+    file_handler = logging.FileHandler(filename=log_filename, encoding='utf-8')
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    return file_handler
+
+
+logger.addHandler(get_stream_handler())
+logger.addHandler(get_file_handler())
+logger.setLevel(logging.DEBUG)
+
+logger.info('===== Start =====')
 my_ipv6_addr = get_public_ipv6_address()
 if my_ipv6_addr == '':
-    logging.info('No IPv6 public address found. Exit now...')
+    logger.info('No IPv6 public address found. Exit now...')
     sys.exit(1)
 
 client = DnsPodCN_DDNSClient(login_token)
-record_ipv6_addr = client.get_AAAA_record_IPv6_address(subdomain=subdomain, domain_name=domain)
+record_ipv6_addr = client.get_AAAA_record_IPv6_address(
+    subdomain=subdomain, domain_name=domain)
 if record_ipv6_addr == '':
-    logging.warning('Can not get record from DDNS API. Exit now...')
+    logger.warning('Can not get record from DDNS API. Exit now...')
     sys.exit(2)
 
 if my_ipv6_addr == record_ipv6_addr:
-    logging.warning(f'Address is not changed {my_ipv6_addr}. No need to update.')
+    logger.warning(f'Address is not changed {
+                   my_ipv6_addr}. No need to update.')
     sys.exit(0)
 
-success = client.update_AAAA_record(my_ipv6_addr, subdomain=subdomain, domain_name=domain)
+success = client.update_AAAA_record(
+    my_ipv6_addr, subdomain=subdomain, domain_name=domain)
 if success:
-    logging.info(f'SUCCESS: Updated the AAAA record for {subdomain}.{domain} to {my_ipv6_addr}')
+    logger.info(f'SUCCESS: Updated the AAAA record for {
+                subdomain}.{domain} to {my_ipv6_addr}')
 else:
-    logging.info(f'ERROR: Failed to update the AAAA record for {subdomain}.{domain} to {my_ipv6_addr}.')
+    logger.info(f'ERROR: Failed to update the AAAA record for {
+                subdomain}.{domain} to {my_ipv6_addr}.')
